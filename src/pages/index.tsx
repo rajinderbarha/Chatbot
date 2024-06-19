e543123cc440a22b4d52062252f6d6d76c977503
rajinderbarha <rajinderbarha@gmail.com>
2024-06-19 10:12:09 +0530

    Webpage is Mobile Responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import Layout from "@/layout";
 import Hero from "@/components/Hero";
 
 const inter = Inter({ subsets: ["latin"] });
